b6eb66e7c589b79898a0ddaeec5bb82ef0b3352c
Kazuki Shimizu <kazubu@jtime.net>
2026-04-13 03:44:43 +0900

    disable HID console by default

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-make crkbd/rev1:via -e USER_NAME=holykeebs -e POINTING_DEVICE=trackpoint -e POINTING_DEVICE_POSITION=right -e OLED=yes -e CONSOLE=yes -j8
-make ai03/altair_x:via -e USER_NAME=holykeebs -e POINTING_DEVICE=trackpoint -e POINTING_DEVICE_POSITION=right -e OLED=no -e CONSOLE=yes -j8
+make crkbd/rev1:via -e USER_NAME=holykeebs -e POINTING_DEVICE=trackpoint -e POINTING_DEVICE_POSITION=right -e OLED=yes -e CONSOLE=no -j8
+make ai03/altair_x:via -e USER_NAME=holykeebs -e POINTING_DEVICE=trackpoint -e POINTING_DEVICE_POSITION=right -e OLED=no -e CONSOLE=no -j8
